66069a5cf0d626206c366a29250aac3aa0bfcfd2
sorochinskkydm <sorochinskkydm@gmail.com>
2023-02-18 17:18:04 +0300

    Работа с циклами

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-/* Задание на урок:
-
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
-
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
-
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
-    }
-
-Проверить, чтобы все работало без ошибок в консоли */
-
 'use strict';
 
 let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?")
@@ -33,13 +10,18 @@ const personalMovieDB = {
     private: false
 }
 
-let film1 = prompt('Один из последних просмотренных фильмов?')
-let someCount1 = +prompt('На сколько оцените его?')
-let film2 = prompt('Один из последних просмотренных фильмов?')
-let someCount2 = +prompt('На сколько оцените его?')
+for (let i = 0; i < 2; i++) {
+    let film1 = prompt('Один из последних просмотренных фильмов?')
+    let someCount1 = +prompt('На сколько оцените его?')
+    if (film1 != null && someCount1 != null && film1 != "" && someCount1 != "" && film1.length < 50) {
+        personalMovieDB.movies[film1] = someCount1 
+    } else i--;
 
-personalMovieDB.movies[film1] = someCount1
-personalMovieDB.movies[film2] = someCount2
-
+    if (personalMovieDB.count < 10) {
+        console.log("Просмотрено мало фильмов")
+    }else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+        console.log("Просмотрено мало фильмов")
+    }else console.log("Вы киноман")
+}
 
 
