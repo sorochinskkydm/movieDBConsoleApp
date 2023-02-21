16e67de77840fd8e305abc9c65139f55586ca12f
sorochinskkydm <sorochinskkydm@gmail.com>
2023-02-21 04:04:32 +0300

    Перенос операций в функции и работа с ними

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 'use strict';
 
-let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?")
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?")
+
+    while(numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?")
+    }
+}
+
+//start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -10,18 +20,47 @@ const personalMovieDB = {
     private: false
 }
 
-for (let i = 0; i < 2; i++) {
-    let film1 = prompt('Один из последних просмотренных фильмов?')
-    let someCount1 = +prompt('На сколько оцените его?')
-    if (film1 != null && someCount1 != null && film1 != "" && someCount1 != "" && film1.length < 50) {
-        personalMovieDB.movies[film1] = someCount1 
-    } else i--;
 
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        let film1 = prompt('Один из последних просмотренных фильмов?')
+        let someCount1 = +prompt('На сколько оцените его?')
+        if (film1 != null && someCount1 != null && film1 != "" && someCount1 != "" && film1.length < 50) {
+            personalMovieDB.movies[film1] = someCount1 
+        } else i--;
+    }
+}
+
+//rememberMyFilms();
+
+function detectPersonalLevel() {
     if (personalMovieDB.count < 10) {
         console.log("Просмотрено мало фильмов")
     }else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
         console.log("Просмотрено мало фильмов")
     }else console.log("Вы киноман")
 }
+
+//detectPersonalLevel();
+
+function showMyDB() {
+    if(personalMovieDB.private == false) {
+        console.log(personalMovieDB);
+    }
+}
+
+function writeYourGenres() {
+    for(let i = 1; i <= 3; i++) {
+        let favouriteGenre = prompt(`Ваш любимый жанр под номером ${i}`);
+        personalMovieDB.genres[i - 1] = favouriteGenre;
+    }
+    console.log(personalMovieDB);
+}
+
+writeYourGenres();
+
+
+
+
 
 
